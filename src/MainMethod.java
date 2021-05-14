43691daba0f9a948e31dccec0a475153ff528627
manish21chintu <manishbm825@gmail.com>
2021-05-14 16:51:39 +0530

    changes commit

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -2,11 +2,19 @@ public class MainMethod {
     public static void main(String[] args) {
         MainMethod.add(10,10);;
         MainMethod.sub(10,10);
+        MainMethod.add1(10,10);;
+        MainMethod.sub1(10,10);
     }
     public static void add(int a,int b){
         System.out.println(a+b);
     }
     public static void sub(int a,int b){
+        System.out.println(b-a);
+    }
+     public static void add1(int a,int b){
+        System.out.println(a+b);
+    }
+    public static void sub1(int a,int b){
         System.out.println(b-a);
     }
 }
