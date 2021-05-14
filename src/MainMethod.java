94e8b36f6472e62d074ff503ec119a3bf0c25b04
mbodla <mbodla@nextsphere.com>
2021-05-14 16:40:40 +0530

    changes

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -1,12 +1,12 @@
 public class MainMethod {
     public static void main(String[] args) {
-       MainMethod.multiply(10,10);
-        MainMethod.divide(10,10);
+        MainMethod.add(10,10);;
+        MainMethod.sub(10,10);
     }
-    public static void multiply(int a,int b){
-    System.out.println(a*b);
+    public static void add(int a,int b){
+        System.out.println(a+b);
     }
-    public static void divide(int a,int b){
-    System.out.println(a/b);
+    public static void sub(int a,int b){
+        System.out.println(b-a);
     }
 }
