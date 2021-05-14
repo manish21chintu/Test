8c5a9812d8bdd2adcabee59adacc13819d7b9032
manish21chintu <manishbm825@gmail.com>
2021-05-14 16:36:05 +0530

    second commit

--- src/MainMethod.java
+++ src/MainMethod.java
@@ -1,5 +1,12 @@
 public class MainMethod {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+       MainMethod.multiply(10,10);
+        MainMethod.divide(10,10);
+    }
+    public static void multiply(int a,int b){
+    System.out.println(a*b);
+    }
+    public static void divide(int a,int b){
+    System.out.println(a/b);
     }
 }
